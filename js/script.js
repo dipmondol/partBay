c86c11e4f1d879b0ca92b47c51e14ad7cea7b873
dipmondol <mondoldip76@gmail.com>
2022-04-28 23:47:15 +0600

    popular parts and why us section

--- js/script.js
+++ js/script.js
@@ -1,42 +1,74 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+
+
+
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-document.querySelector('.login-btn').onclick =   () => {
-    document.querySelector('.login-form-container').classList.toggle('active');
-}
-document.querySelector('#login-form-close').onclick =   () => {
-    document.querySelector('.login-form-container').classList.remove('active');
-}
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+document.querySelector(".login-btn").onclick = () => {
+  document.querySelector(".login-form-container").classList.toggle("active");
+};
+document.querySelector("#login-form-close").onclick = () => {
+  document.querySelector(".login-form-container").classList.remove("active");
+};
 
-window.onscroll = () =>{
-    if(window.scrollY > 0 ){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-    menu.classList.remove('fa-times');
-    menu.classList.remove('active');
-}
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+  menu.classList.remove("fa-times");
+  menu.classList.remove("active");
+};
 // Banner hover
-document.querySelector('.home').onmousemove = (e) => {
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
-        
-        let speed = elm.getAttribute('data-speed');
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
+document.querySelector(".home").onmousemove = (e) => {
+  document.querySelectorAll(".home-parallax").forEach((elm) => {
+    let speed = elm.getAttribute("data-speed");
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
 
-        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
-    });
-}
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+  });
+};
 
-document.querySelector('.home').onmouseleave = () => {
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
-        
-       
-        elm.style.transform = `translateX(0px) translateY(0px)`;
-    });
-}
+document.querySelector(".home").onmouseleave = () => {
+  document.querySelectorAll(".home-parallax").forEach((elm) => {
+    elm.style.transform = `translateX(0px) translateY(0px)`;
+  });
+};
+
+
+var swiper = new Swiper(".vehicles-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
