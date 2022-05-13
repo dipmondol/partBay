e0b27cfd75a93aec97ccdf427261532f146aba63
dipmondol <mondoldip76@gmail.com>
2022-05-14 00:44:21 +0600

    add dark mode feature

--- js/adminPanel.js
+++ js/adminPanel.js
@@ -1,6 +1,9 @@
 const sideMenu = document.querySelector('.sideBarMenu');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector('.theme-toggler');
+
+
 
 
 menuBtn.addEventListener('click', () =>{
@@ -11,4 +14,10 @@ menuBtn.addEventListener('click', () =>{
     sideMenu.style.display = 'none';
 
 }) 
-  
+
+ themeToggler.addEventListener('click', () =>{
+      document.body.classList.toggle('dark-theme');
+
+      themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+      themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+  })
