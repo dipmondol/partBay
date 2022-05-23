ad80e14a375daa4ff517dc6520e6407942b929ab
dipmondol <mondoldip76@gmail.com>
2022-05-24 01:16:34 +0600

    add date and time feature for admin panel

---
+++ js/dateTime.js
@@ -0,0 +1,39 @@
+function updateClock(){
+    var now = new Date();
+    var dateName = now.getDay(),
+    month = now.getMonth(),
+    dateNum = now.getDate(),
+    year = now.getFullYear(),
+    hour = now.getHours(),
+    min = now.getMinutes(),
+    sec = now.getSeconds(),
+    pe = "AM";
+
+
+
+    if(hour == 0){
+        hour = 12;
+    }
+    if(hour > 12 ){
+        hour = hour - 12;
+        pe = "PM";
+    }
+    Number.prototype.pad = function(digits){
+        for(var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    }
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octobor", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids =["dayName", "month", "dayNum", "year", "hour", "minutes", "seconds", "period"];
+    var values =[ week[dateName], months[month], dateNum.pad(2), year, hour.pad(2), min.pad(2), sec.pad(2), pe];
+
+    for(var i = 0; i < ids.length; i++){
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+    }
+
+}
+function initClock(){
+    updateClock();
+    window.setInterval("updateClock()", 1);
+}
