af76d3e09e4f9f9533d25549378172bc28673804
dipmondol <mondoldip76@gmail.com>
2022-05-28 00:07:15 +0600

    chart customization

--- js/chart.js
+++ js/chart.js
@@ -2,17 +2,17 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Fender Liner', 'Headlights', 'Door Handle', 'Washer Reservoir', 'Tail Lamp', 'Mirror'],
         datasets: [{
-            label: '# of Votes',
+            label: 'Parts Sale Progress',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 206, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(153, 102, 255, 08)',
+                'rgba(255, 159, 64, 0.8)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
